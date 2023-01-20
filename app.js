a1a9ed44f1697d9e63637ac7587ec975e250b00f
ajdin <ajdinhusetovic@outlook.com>
2023-01-20 21:03:14 +0100

    left arrow and right arrow button logic redone, added basic styling like background colors and text colors

--- app.js
+++ app.js
@@ -10,25 +10,36 @@ images[1] = 'images/photo2.jpg';
 images[2] = 'https://images.freeimages.com/images/large-previews/de9/cat-laying-on-chair-1640758.jpg';
 images[3] = 'https://images.freeimages.com/images/large-previews/785/red-bird-in-the-sunshine-1641857.jpg';
 
+imageHTML.src = images[0];
 
-
-let i = 0;
+let id = 0;
 
 rightButton.addEventListener('click', () => {
 
-    if (i < images.length) {
-        i++;
-        imageHTML.src = images[i];
+    // increases id by 1 with each click
+    id++;
+
+    // checks if the id is on the last image
+    if (id > images.length - 1) {
+        id = 0;
     }
+
+    imageHTML.src = images[id];
 
 });
 
 leftButton.addEventListener('click', () => {
 
-    if (i === images.length || i < images.length){
-        i--;
-        imageHTML.src = images[i];
+    // decreases id by 1 with each click
+    id--;
+
+    // makes sure that id doesn't go below index 0
+    if (id < 0) {
+        id = images.length - 1; 
     }
+
+    imageHTML.src = images[id];
+     
 });
 
 
